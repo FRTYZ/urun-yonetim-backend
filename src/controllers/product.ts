1fc659bf8ebffca5b36d742884a24a7be91c55a3
Fırat YILDIZ <firatyildiznet@gmail.com>
2024-09-24 15:42:25 +0300

    Created postProduct api service

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -3,13 +3,59 @@ import { Request, Response, NextFunction } from 'express';
 // Models
 const Products = require('../models/products'); 
 
+// Error 
+const CustomError = require('../errors/CustomError');
+
+// Helpers
+const File = require('../helpers/fileUpload');
+
 exports.getProduct = async function (req: Request, res: Response, next: NextFunction) {
     try {
         const values = await Products.find({});
 
         return res.status(200).json(values);
     }catch (err){
-        console.log(err)
+        next(err)
+    }
+}
+
+exports.postProduct = async function (req: Request, res: Response, next: NextFunction) {
+    
+    try {
+        const {name, description, price, stock} = req.body;
+        const getImage = req.file;
+
+        if(!getImage){
+            throw new CustomError(403, "select the product picture field");
+        }
+        if(!name || name == ''){
+            throw new CustomError(403, "You need to specify the name field.");
+        }
+        if(!description || name == ''){
+            throw new CustomError(403, "You need to specify the description field.");
+        }
+        if(!price || price == ''){
+            throw new CustomError(403, "You need to specify the price field.");
+        }
+        if(!stock || stock == ''){
+            throw new CustomError(403, "You need to specify the stock field.");
+        }
+
+        const featuredImage = await File.uploadFile(getImage, 'products/' + name  + '/' );
+
+        const newProduct = new Products({ 
+            name,
+            description,
+            price,
+            stock,
+            featuredImage
+        });
+        
+        await newProduct.save();
+
+        return res.status(201).json({ sucess: true })
+
+    }catch (err){
         next(err)
     }
 }
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -4,6 +4,10 @@ const router = express.Router();
 // Controllers
 const productController = require("../controllers/product");
 
+// Helpers
+const multer = require('../helpers/multer');
+
 router.get("/list", productController.getProduct);
+router.post("/list", multer.file_upload.single('featuredImage'), productController.postProduct, multer.body_parse.array());
 
 module.exports = router;
